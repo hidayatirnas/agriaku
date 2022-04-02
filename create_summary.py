6b6300b97600c086ddc42ccee65036c4e5ea8318
hidayat <hidayat.hidayat@accenture.com>
2022-04-03 01:35:02 +0700

    fix: Continue to create the summary until the percentage

--- create_summary.py
+++ create_summary.py
@@ -11,6 +11,11 @@ df_final = df_final.merge(df_dim_course, left_on="course_id", right_on="ID", how
 
 # do transformations
 df_final = df_final[["student_id", "week", "semester", "NAME", "target", "actual"]]
-df_final.rename(columns={"NAME": "course_name"}, inplace=True)
+df_final.rename(columns={"semester": "SEMESTER_ID", "week": "WEEK_ID", "NAME": "COURSE_NAME"}, inplace=True)
 
-df_final.to_csv("summary.csv", index=False)
+# prepare the summary
+df_summary = df_final.groupby(by=["SEMESTER_ID", "COURSE_NAME", "WEEK_ID"])[["target", "actual"]].sum()
+df_summary["ATTENDANCE_PCT"] = df_summary["actual"] / df_summary["target"] * 100
+df_summary.drop(columns=["target", "actual"], inplace=True)
+
+df_summary.to_csv("summary.csv", index=True)
